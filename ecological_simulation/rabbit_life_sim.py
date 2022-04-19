7a6f2a19bd6b45eda8d449c86e478486de5944e4
Sreevatsa03 <sreevatsa.nukala@gmail.com>
2022-04-19 17:10:38 -0400

    added working color map and user inputted command line arguments

--- ecological_simulation/rabbit_life_sim.py
+++ ecological_simulation/rabbit_life_sim.py
@@ -5,17 +5,17 @@ import matplotlib.colors as colors
 import numpy as np
 import copy
 import sys
-from IPython import display
+from argparse import ArgumentParser, ArgumentError
 
 
 # Does the field wrap around on itself when rabbits move?
 WRAP = True
 
 # colors
-WHITE = 0
-GREEN = 1
-BLUE = 2
-RED = 3
+WHITE = 0  # empty space
+GREEN = 1  # grass
+BLUE = 2  # pygmy
+RED = 3  # cottontail
 
 
 class Rabbit:
@@ -47,8 +47,10 @@ class Rabbit:
     def move(self):
         """ Move up, down, left, right randomly """
 
+        # create a list of possible moves in integer set: {-max_hop_distance, ..., max_hop_distance}
         mv_lst = list(range(-self.hop_dist, self.hop_dist + 1))
 
+        # move rabbit
         if WRAP:
             self.x = (self.x + rnd.choice(mv_lst)) % self.size
             self.y = (self.y + rnd.choice(mv_lst)) % self.size
@@ -59,21 +61,21 @@ class Rabbit:
 
 class Pygmy(Rabbit):
 
-    def __init__(self):
-        super().__init__(max_offspring=2)
+    def __init__(self, size=300):
+        super().__init__(max_offspring=2, field_size=size)
 
 
 class CottonTail(Rabbit):
 
-    def __init__(self):
-        super().__init__(max_hop_distance=2, color=RED)
+    def __init__(self, size=300):
+        super().__init__(max_hop_distance=2, color=RED, field_size=size)
 
 
 class Field:
     """ A field is a patch of grass with 0 or more rabbits hopping around
     in search of grass """
 
-    def __init__(self, field_size=300, grass_rate=0.04, num_pygmy=1, num_cotton_tail=1):
+    def __init__(self, field_size=300, grass_rate=0.1, num_pygmy=1, num_cotton_tail=1):
         """ Create a patch of grass with dimensions size x size
         and initially no rabbits """
 
@@ -87,8 +89,9 @@ class Field:
         self.field = np.ones(shape=(self.size, self.size), dtype=int)
 
         # initialize rabbits
-        self.rabbits = [Pygmy() for _ in range(num_pygmy)]
-        self.rabbits += [CottonTail() for _ in range(num_cotton_tail)]
+        self.rabbits = [Pygmy(self.size) for _ in range(num_pygmy)]
+        self.rabbits.extend([CottonTail(self.size)
+                            for _ in range(num_cotton_tail)])
 
         # keep track of number of rabbits per species and amount of grass
         self.npygmy = [num_pygmy]
@@ -96,24 +99,30 @@ class Field:
         self.ngrass = [self.size * self.size]
 
         # create colormap
-        cmap = colors.ListedColormap(["red", "blue", "white", "green"])
-        # cmap = colors.ListedColormap(["white", "green", "blue", "red"])
+        cmap = colors.ListedColormap(["white", "green", "blue", "red"])
 
-        # plot of field to use for animation
-        # plot_field = copy.deepcopy(self.field)
-        # for i in range(len(plot_field)):
-        #     for j in range(len(plot_field[i])):
-        #         if plot_field[i][j] ==
+        # set field to plot
+        self._update_plot()
 
+        # set image to animate
         self.fig = plt.figure(figsize=(5, 5))
-        self.im = plt.imshow(self.field, cmap=cmap,
-                             aspect='auto', vmin=0, vmax=1)
+        self.im = plt.imshow(self.plot, cmap=cmap,
+                             aspect='auto', vmin=0, vmax=3)
+
+    def _update_plot(self):
+        """ Update field to plot """
+
+        # pygmy and cottontail arrays in dict
+        rabbit_dict = self._get_rabbits()
+
+        # plot of field to use for field animation
+        self.plot = np.maximum.reduce(
+            [self.field, rabbit_dict['pygmy'], rabbit_dict['cotton']])
 
     def _move(self):
         """ Rabbits move """
         for r in self.rabbits:
             r.move()
-            # self.field[r.x, r.y] = r.color
 
     def _eat(self):
         """ Rabbits eat (if they find grass where they are) """
@@ -146,10 +155,19 @@ class Field:
         self.field = np.maximum(self.field, growloc)
 
     def _get_rabbits(self):
-        rabbits = np.zeros(shape=(self.size, self.size), dtype=int)
+        """ Get arrays of pygmy and cottontail rabbits to use in plotting """
+
+        # initialize 2 arrays to keep track of both rabbit populations
+        pygmy = np.zeros(shape=(self.size, self.size), dtype=int)
+        cotton = np.zeros(shape=(self.size, self.size), dtype=int)
+
+        # add rabbits' colors (2 or 3) to each numpy array at their position
         for r in self.rabbits:
-            rabbits[r.x, r.y] = 1
-        return rabbits
+            if r.color == 2:
+                pygmy[r.x, r.y] = r.color
+            elif r.color == 3:
+                cotton[r.x, r.y] = r.color
+        return {'pygmy': pygmy, 'cotton': cotton}
 
     def _num_rabbits(self):
         """ How many rabbits are there in the field ? """
@@ -158,11 +176,14 @@ class Field:
         return rabbit_list.count(2), rabbit_list.count(3)
 
     def _amount_of_grass(self):
+        """ calculate how much grass is currently on field """
+
         num_grass = np.sum(self.field[self.field == 1])
         return num_grass
 
     def _generation(self):
         """ Run one generation of rabbits """
+
         self._move()
         self._eat()
         self._survive()
@@ -176,24 +197,32 @@ class Field:
         for _ in range(speed):
             self._generation()
 
+        # update plot of field
+        self._update_plot()
+
         # Update the frame
-        self.im.set_array(self.field)
+        self.im.set_array(self.plot)
         plt.title("generation = " + str((i + 1) * speed))
         return self.im,
 
     def run(self, generations=5000, speed=1):
         """ Run the simulation. Speed denotes how may generations run between successive frames """
+
         anim = animation.FuncAnimation(self.fig, self._animate, fargs=(
             speed, ), frames=generations // speed, interval=1, repeat=False)
         plt.show()
 
     @staticmethod
     def _update(num, x, y, z, line1, line2):
+        """ Update array by one iteration for building line plot animation (history) """
+
         line1.set_data(x[:num], y[:num])
         line2.set_data(x[:num], z[:num])
         return [line1, line2]
 
     def history(self, speed=1, marker='.'):
+        """ Animated line plot of Pygmy and CottonTail populations over generations """
+
         # initialize plot
         fig, ax = plt.subplots()
 
@@ -216,10 +245,46 @@ class Field:
         plt.ylabel("# Rabbits")
         plt.title("Pygmy Rabbits vs. Cotton Tail Rabbits per Generation")
         plt.savefig("pyg_vs_cot_hist.png", bbox_inches='tight')
-        ani.save("pyg_vs_cot_hist.gif", dpi=300, writer=animation.PillowWriter(fps=25))
+        ani.save("pyg_vs_cot_hist.gif", dpi=300,
+                 writer=animation.PillowWriter(fps=25))
         plt.show()
 
-    def history2(self, marker='o'):
+    @staticmethod
+    def _update2(num, x, y, line):
+        """ Update array by one iteration for building line plot animation (history2) """
+
+        line.set_data(x[:num], y[:num])
+        return [line]
+
+    def history2(self, speed=1, marker='.'):
+        """ Animated line plot of Pygmy vs. CottonTail populations """
+
+        # initialize plot
+        fig, ax = plt.subplots()
+
+        # set lists to plot
+        pygmy = np.array(self.npygmy[:])
+        cotton = np.array(self.ncotton[:])
+
+        # plot figure
+        line, = ax.plot(pygmy, cotton, color="purple", marker=marker)
+
+        ani = animation.FuncAnimation(fig, self._update2, len(pygmy), fargs=[
+                                      pygmy, cotton, line], interval=speed, repeat=False, blit=False)
+
+        # configure plot and display
+        plt.grid()
+        plt.xlabel("# Pygmy Rabbits")
+        plt.ylabel("# CottonTail Rabbits")
+        plt.title("Pygmy Rabbits Population vs. Cotton Tail Rabbits Population")
+        plt.savefig("pyg_vs_cot_hist2.png", bbox_inches='tight')
+        ani.save("pyg_vs_cot_hist2.gif", dpi=300,
+                 writer=animation.PillowWriter(fps=25))
+        plt.show()
+
+    def history3(self, marker='o'):
+        """ 3D plot of Pygmy vs. CottonTail vs. Grass populations """
+
         # initialize 3D plot
         fig = plt.figure(figsize=(6, 6))
         ax = fig.add_subplot(projection='3d')
@@ -243,23 +308,37 @@ class Field:
 
 
 def main():
+    # parse user defined parameters from command line
+    # $ python rabbit_life_sim.py <field size> <initial pygmy> <initial cottontail> <sim speed> --gens <num generations> --grass <grass growth rate>
+    parser = ArgumentParser()
+    parser.add_argument('size', type=int, help='field size')
+    parser.add_argument('pygmy', type=int, help='intitial pygmy population')
+    parser.add_argument('cotton', type=int,
+                        help='initial cottontail population')
+    parser.add_argument('speed', type=int, help='simulation speed')
+    parser.add_argument('--gens', type=int, help='number of generations')
+    parser.add_argument('--grass', type=float, help='grass growth rate')
 
-    if len(sys.argv) != 7:
-
-        # run ex: python rabbit_life_sim.py 300 0.04 10 10 1000 10
-        print("USAGE: $ python rabbit_life_sim.py <field size> <grass growth rate> <initial pygmy> <initial cottontail> <num generations> <sim speed>")
-        sys.exit()
+    args = parser.parse_args()
 
     # Create the ecosystem
-    field = Field(field_size=int(sys.argv[1]), grass_rate=float(
-        sys.argv[2]), num_pygmy=int(sys.argv[3]), num_cotton_tail=int(sys.argv[4]))
+    if args.grass is None:
+        field = Field(field_size=args.size, num_pygmy=args.pygmy,
+                      num_cotton_tail=args.cotton)
+    else:
+        field = Field(field_size=args.size, grass_rate=args.grass,
+                      num_pygmy=args.pygmy, num_cotton_tail=args.cotton)
 
     # Run the ecosystem
-    field.run(generations=int(sys.argv[5]), speed=int(sys.argv[6]))
+    if args.gens is None:
+        field.run(speed=args.speed)
+    else:
+        field.run(generations=args.gens, speed=args.speed)
 
     # Plot history
-    field.history(speed=int(sys.argv[6]))
-    field.history2()
+    field.history(speed=args.speed)
+    field.history2(speed=args.speed)
+    field.history3()
 
 
 if __name__ == '__main__':
